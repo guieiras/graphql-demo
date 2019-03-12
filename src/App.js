71ea676ffffa507f6a527f39c7c3b7a4a4d5ce9e
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-03-12 20:46:28 -0300

    Added containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="rest">
+        
+        </div>
+        <div className="graphql">
+        
+        </div>
       </div>
     );
   }
